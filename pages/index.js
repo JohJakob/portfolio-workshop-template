0f0456112dc41c46862934b1c8ac3cf7a36da678
Johannes Jakob <hi@johjakob.com>
2020-09-17 18:23:34 +0200

    feat: Add index page

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head"
+import { Component } from "react"
+import Link from "next/link"
+
+export default class Index extends Component {
+  render() {
+    return (
+      <>
+        <Head>
+          <title>Build Your Online Portfolio – Website Template</title>
+        </Head>
+        <main>
+          <h1>Hello World</h1>
+        </main>
+      </>
+    )
+  }
+}
