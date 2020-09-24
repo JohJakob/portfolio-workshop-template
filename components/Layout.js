641230d745d2250d60565d7c93c9286f0308c33c
Johannes Jakob <hi@johjakob.com>
2020-09-24 22:22:44 +0200

    feat: Add meta tags to Layout component

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,15 @@
+import Head from "next/head"
 import Header from "./Header"
 import Footer from "./Footer"
 
 function Layout({ children }) {
   return (
     <>
+      <Head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Header />
       <main>
         {children}
